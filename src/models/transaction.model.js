2b904574083c23f13b8c90325d1314ae491a4629
Arslansaifi012 <devarslan457@gmail.com>
2026-09-10 19:12:04 +0530

    add some line of code

--- src/models/transaction.model.js
+++ src/models/transaction.model.js
@@ -20,17 +20,24 @@ const transactionSchema = new mongoose.Schema({
 
    status:{
     type:String,
-
     enum:{
         values: ["PENDING", "COMPLETED","FAILED","REVERSED"],
         message:"status can be either PENDING, COMPLETED, FAILED, or REVERSED",
         default:"PENDING"
-    },
+    }},
 
-    amount:{
+     amount:{
         type:Number,
         required:[true, "Amount is required for creating a transaction"],
         min:[0, "Transaction amount cannot be negative"]
-    }
-   }
+    },
+
+    idempotencyKey:{
+        type:String,
+        required: [true, "Idempotency key is required for creating a transaction"],
+        index:true,
+        unique:true,
+    },
+},{
+    timestamps:true
 }) ;
